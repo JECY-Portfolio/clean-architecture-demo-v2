5f0db7b3f0df4197c6ea47e0eef4e68a46984322
Onyedikachi Geoffrey <jecintageoffrey@yahoo.com>
2025-11-14 12:31:43 +0100

    Set up Clean Architecture Foundation with CQRS and MediatR for Initial Blog CRUD

--- clean-architecture-demo-v2.Api/Program.cs
+++ clean-architecture-demo-v2.Api/Program.cs
@@ -1,6 +1,13 @@
+using clean_architecture_demo_v2.Application;
+using clean_architecture_demo_v2.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Add layers dependencies
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ clean-architecture-demo-v2.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace clean_architecture_demo_v2.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
