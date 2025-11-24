757531534ab4a297fdd27b7530beae33fe310456
Onyedikachi Geoffrey <jecintageoffrey@yahoo.com>
2025-11-24 23:38:17 +0100

    Implement Infrastructure Layer and DbContext in DI

--- clean-architecture-demo-v2.Infrastructure/ConfigureServices.cs
+++ clean-architecture-demo-v2.Infrastructure/ConfigureServices.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using clean_architecture_demo_v2.Domain.Repository;
+using clean_architecture_demo_v2.Infrastructure.Data;
+using clean_architecture_demo_v2.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace clean_architecture_demo_v2.Infrastructure
@@ -7,6 +11,12 @@ namespace clean_architecture_demo_v2.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<BlogDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("BlogDbContext") ??
+                    throw new InvalidOperationException("Connection string 'BlogDbContext not found'"))
+            );
+
+            services.AddTransient<IBlogRepository, BlogRepository>();
             return services;
         }
     }
